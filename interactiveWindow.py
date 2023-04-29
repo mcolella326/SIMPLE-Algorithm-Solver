f2ed874f0533770411bdc80ea1a3e88f58ca07e5
Michael <mcolella326@gmail.com>
2023-04-29 13:57:05 -0400

    Accounted for dynamic zoom and dynamic prism dimensions

--- interactiveWindow.py
+++ interactiveWindow.py
@@ -2,19 +2,21 @@ import pygame
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-def draw_cube(num_height_subdivisions=1, num_length_subdivisions=1, num_width_subdivisions=1):
-    # Set the color of all faces to red
+def draw_cube(height, length, width, num_height_subdivisions=1, num_length_subdivisions=1, num_width_subdivisions=1):
+    # Set the color of all faces to gray
     glColor3f(0.5, 0.5, 0.5)
 
     # Define vertices and indices
-    vertices = [(-1, -1, -1),
-                (1, -1, -1),
-                (1, 1, -1),
-                (-1, 1, -1),
-                (-1, -1, 1),
-                (1, -1, 1),
-                (1, 1, 1),
-                (-1, 1, 1)]
+
+    # Vertices defined in (length, height, width) order
+    vertices = [(-length / 2, -height / 2, -width / 2),
+                (length / 2, -height / 2, -width / 2),
+                (length / 2, height / 2, -width / 2),
+                (-length / 2, height / 2, -width / 2),
+                (-length / 2, -height / 2, width / 2),
+                (length / 2, -height / 2, width / 2),
+                (length / 2, height / 2, width / 2),
+                (-length / 2, height / 2, width / 2)]
     
     indices = [(0, 1, 2, 3),
                (1, 5, 6, 2),
@@ -23,33 +25,33 @@ def draw_cube(num_height_subdivisions=1, num_length_subdivisions=1, num_width_su
                (4, 5, 1, 0),
                (3, 2, 6, 7)]
 
-    # draw the faces
+    # Draw the faces
     glBegin(GL_QUADS)
     for index_face in indices:
         for vertex_ind in index_face:
             glVertex3fv(vertices[vertex_ind])
     glEnd()
 
-    # set the color of edges to black
+    # Set the color of edges to black and adjust polygon mode to get desired projection mode
     glColor3f(0, 0, 0)
     glEnable(GL_DEPTH_TEST)
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-    glLineWidth(2.0)  # set line width
+    glLineWidth(2.0)
 
     # Calculate subdivision sizes
-    height_subdivision_size = 2 / (num_height_subdivisions)
-    length_subdivision_size = 2 / (num_length_subdivisions)
-    width_subdivision_size = 2 / (num_width_subdivisions)
+    height_subdivision_size = height / (num_height_subdivisions)
+    length_subdivision_size = length / (num_length_subdivisions)
+    width_subdivision_size = width / (num_width_subdivisions)
 
     # Draw subdivided edges along height dimension
     glBegin(GL_LINES)
-    for i in range(num_height_subdivisions):
-        z = -1 + i*height_subdivision_size
+    for i in range(num_width_subdivisions):
+        z = (-width / 2) + i*width_subdivision_size
         for j in range(num_length_subdivisions):
-            x1 = -1 + j*length_subdivision_size
+            x1 = (-length / 2) + j*length_subdivision_size
             x2 = x1 + length_subdivision_size
-            y1 = -1
-            y2 = 1
+            y1 = -height / 2
+            y2 = height / 2
             glVertex3f(x1, y1, z)
             glVertex3f(x2, y1, z)
             glVertex3f(x2, y1, z)
@@ -63,12 +65,12 @@ def draw_cube(num_height_subdivisions=1, num_length_subdivisions=1, num_width_su
     # Draw subdivided edges along length dimension
     glBegin(GL_LINES)
     for i in range(num_length_subdivisions):
-        x = -1 + i*length_subdivision_size
-        for j in range(num_width_subdivisions):
-            y1 = -1 + j*width_subdivision_size
-            y2 = y1 + width_subdivision_size
-            z1 = -1
-            z2 = 1
+        x = (-length / 2) + i*length_subdivision_size
+        for j in range(num_height_subdivisions):
+            y1 = (-height / 2) + j*height_subdivision_size
+            y2 = y1 + height_subdivision_size
+            z1 = -width / 2
+            z2 = width / 2
             glVertex3f(x, y1, z1)
             glVertex3f(x, y1, z2)
             glVertex3f(x, y1, z2)
@@ -81,13 +83,13 @@ def draw_cube(num_height_subdivisions=1, num_length_subdivisions=1, num_width_su
 
     # Draw subdivided edges along width dimension
     glBegin(GL_LINES)
-    for i in range(num_width_subdivisions):
-        y = -1 + i*width_subdivision_size
-        for j in range(num_height_subdivisions):
-            z1 = -1 + j*height_subdivision_size
-            z2 = z1 + height_subdivision_size
-            x1 = -1
-            x2 = 1
+    for i in range(num_height_subdivisions):
+        y = (-height / 2) + i*height_subdivision_size
+        for j in range(num_width_subdivisions):
+            z1 = (-width / 2) + j*width_subdivision_size
+            z2 = z1 + width_subdivision_size
+            x1 = -length / 2
+            x2 = length / 2
             glVertex3f(x1, y, z1)
             glVertex3f(x1, y, z2)
             glVertex3f(x1, y, z2)
@@ -104,21 +106,21 @@ def draw_axes():
         # Draw the x-axis
     glColor3f(1.0, 0.0, 0.0)
     glBegin(GL_LINES)
-    glVertex3f(-2.5, 0.0, 0.0)
+    glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(2.5, 0.0, 0.0)
     glEnd()
 
     # Draw the y-axis
     glColor3f(0.0, 1.0, 0.0)
     glBegin(GL_LINES)
-    glVertex3f(0.0, -2.5, 0.0)
+    glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(0.0, 2.5, 0.0)
     glEnd()
 
     # Draw the z-axis
     glColor3f(0.0, 0.0, 1.0)
     glBegin(GL_LINES)
-    glVertex3f(0.0, 0.0, -2.5)
+    glVertex3f(0.0, 0.0, 0.0)
     glVertex3f(0.0, 0.0, 2.5)
     glEnd()
 
@@ -127,8 +129,11 @@ def main():
     display = (1200, 800)
     pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL)
     pygame.display.set_caption('Geometry, Mesh, and Boundary Conditions')
+    length = 1
+    height = 5
+    width = 10
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 5000.0)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50000.0)
 
     glClearColor(1.0, 1.0, 1.0, 1.0)
 
@@ -138,7 +143,7 @@ def main():
     mouse_position = None
     cube_dx, cube_dy = 0, 0
     axis_dx, axis_dy = 0, 0
-    zoom = 0
+    zoom = 5
 
     while True:
         for event in pygame.event.get():
@@ -153,7 +158,10 @@ def main():
                 if event.button == 1:
                     drag = False
             elif event.type == pygame.MOUSEWHEEL:
-                zoom -= event.x*0.1
+                if event.y > 0:
+                    zoom -= 0.1
+                else:
+                    zoom += 0.1
 
         if drag:
             new_mouse_position = pygame.mouse.get_pos()
@@ -183,13 +191,13 @@ def main():
         glViewport(0, 0, display[0], display[1])
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+        gluPerspective(45, (display[0]/display[1]), 0.1, 50000.0)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        glTranslatef(0.0, 0.0, -5)
+        glTranslatef(0.0, 0.0, -max(length, height, width)-zoom)
         glRotatef(cube_dy+35.264, 1, 0, 0)
         glRotatef(cube_dx-45, 0, 1, 0)
-        draw_cube(10, 10, 10)
+        draw_cube(height, length, width, 10, 10, 10)
 
         pygame.display.flip()
 
